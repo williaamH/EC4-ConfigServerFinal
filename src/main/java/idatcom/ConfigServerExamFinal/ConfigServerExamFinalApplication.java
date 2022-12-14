bd3d317f733f224cb26d78c441b7cb2d07ec8f3e
williaamH <william_03_07@hotmail.com>
2022-12-13 22:08:25 -0500

    Config Server EC4

---
+++ src/main/java/idatcom/ConfigServerExamFinal/ConfigServerExamFinalApplication.java
@@ -0,0 +1,15 @@
+package idatcom.ConfigServerExamFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerExamFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerExamFinalApplication.class, args);
+	}
+
+}
